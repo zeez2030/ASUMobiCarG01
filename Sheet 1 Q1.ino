317029550d5201b83001eeb1d3b485734c138ed8
mostafa ibrahim <zeezrdq@gmail.com>
2018-02-19 00:29:04 +0200

    Sheet 1 control introduction to embedded system

--- Sheet 1 Q1.ino
+++ Sheet 1 Q1.ino
@@ -1,9 +1,19 @@
+
+int ledpin= 13;
 void setup() {
   // put your setup code here, to run once:
-int car=0;
+pinMode(ledpin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+digitalWrite(ledpin,HIGH);
+
+delay(1000);
+
+digitalWrite(ledpin,LOW);
+
+delay(3000);
+
 
 }
