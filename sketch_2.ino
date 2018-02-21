ff0d15b8775b2d1104b9e6aaa75880d2117612da
mohamedgamalm <36610546+mohamedgamalm@users.noreply.github.com>
2018-02-21 20:21:40 +0200

    Add files via upload

---
+++ sketch_2.ino
@@ -0,0 +1,36 @@
+
+int LED[]={4,5,6,7};
+
+
+void setup()
+{
+for(int i=4 ; i<8;i++)
+{
+  pinMode(LED[i],OUTPUT);
+  
+}
+
+
+}
+
+void loop()
+{
+ for(int i=4;i<8;i++)
+ {
+   digitalWrite(LED[i],HIGH);
+   delay(300);
+   digitalWrite(LED[i],LOW);
+   delay(300);
+   
+ }
+ for(int i=8;i>4;i--)
+ {
+   digitalWrite(LED[i],HIGH);
+   delay(300);
+   digitalWrite(LED[i],LOW);
+   delay(300);
+   
+ }
+ 
+
+}
